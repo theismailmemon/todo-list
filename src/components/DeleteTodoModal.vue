a41fea65f8b854b3187e6c43cd366fc8c314ed16
Ismail Memon <iamismailmemon@gmail.com>
2024-05-20 13:39:07 -0700

    first commit

--- src/components/DeleteTodoModal.vue
+++ src/components/DeleteTodoModal.vue
@@ -3,11 +3,11 @@
     class="fixed inset-0 z-[100] flex items-center justify-center bg-gray-900 bg-opacity-50">
     <div class="bg-white max-w-auto max-w-full mx-5 p-6 rounded-lg shadow-xl" @click.stop>
       <h2 class="text-xl text-gray-700 font-semibold mb-4">Confirm Deletion</h2>
-      <p>Are you sure you want to delete "{{ todoToDelete.name }}"?</p>
+      <p>Are you sure you want to delete <span class="font-bold text-gray-800">{{ todoToDelete.name }}</span> ?</p>
       <div class="flex justify-end mt-4">
-        <button @click="cancelDelete" class="px-4 py-2 bg-red-500 text-white hover:opacity-80 transition ease-in-out duration-300 rounded-md mr-2">Cancel</button>
+        <button @click="cancelDelete" class="px-4 py-2 border border-blue-950 hover:bg-blue-950 hover:text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md mr-2">Cancel</button>
         <button @click="confirmDelete"
-          class="px-4 py-2 w-20 flex justify-center items-center bg-green-500 text-white hover:opacity-80 transition ease-in-out duration-300 rounded-md"
+          class="px-4 py-2 w-20 flex justify-center items-center border border-blue-950 bg-blue-950 text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md"
           :class="{ 'opacity-60 pointer-events-none': loadingAnimation }">
           <span v-if="!loadingAnimation">Delete</span>
           <div class="flex flex-row space-x-4" v-else>
