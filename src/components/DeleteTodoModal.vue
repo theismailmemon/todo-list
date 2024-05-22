b36b5618d7308f3e55757dcb87ebc2a7136f1e14
Ismail Memon <iamismailmemon@gmail.com>
2024-05-22 15:34:33 -0700

    first commit

--- src/components/DeleteTodoModal.vue
+++ src/components/DeleteTodoModal.vue
@@ -7,12 +7,9 @@
       <div class="flex justify-end gap-4 mt-4">
         <button @click="cancelDelete" class="px-4 py-[8px] w-20 border border-blue-950 bg-blue-950 text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md">Cancel</button>
         <button @click="confirmDelete"
-          class=" px-4 py-[8px] border border-blue-950 hover:bg-blue-950 w-20 flex justify-center items-center hover:text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md"
-          :class="{ 'opacity-60 pointer-events-none': loadingAnimation }">
-          <span v-if="!loadingAnimation">Delete</span>
-          <div class="flex flex-row space-x-4" v-else>
-            <div class="w-6 h-6 rounded-full animate-spin border border-solid border-blue-950 border-t-transparent"></div>
-          </div>
+          class=" px-4 py-[8px] border border-blue-950 hover:bg-blue-950 w-20 flex justify-center items-center hover:text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md">
+          <span>Delete</span>
+        
         </button>
 
       </div>
@@ -24,7 +21,7 @@
 export default {
   data() {
     return {
-      loadingAnimation: false
+  
     }
   },
   props: {
@@ -39,11 +36,7 @@ export default {
       this.$emit('cancel-delete');
     },
     confirmDelete() {
-      this.loadingAnimation = true;
-      setTimeout(() => {
-        this.loadingAnimation = false;
         this.$emit('confirm-delete', this.todoToDelete);
-      }, 1000); // Replace 1000 with the desired delay in milliseconds
     }
 
   }
