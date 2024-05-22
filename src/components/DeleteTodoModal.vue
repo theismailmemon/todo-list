73375c23fa1152db5b72d130a8f4311176480b52
Ismail Memon <iamismailmemon@gmail.com>
2024-05-22 09:44:36 -0700

    first commit

--- src/components/DeleteTodoModal.vue
+++ src/components/DeleteTodoModal.vue
@@ -4,14 +4,14 @@
     <div class="bg-white max-w-auto max-w-full mx-5 p-6 rounded-lg shadow-xl" @click.stop>
       <h2 class="text-xl text-gray-700 font-semibold mb-4">Confirm Deletion</h2>
       <p>Are you sure you want to delete <span class="font-bold text-gray-800">{{ todoToDelete.name }}</span> ?</p>
-      <div class="flex justify-end mt-4">
-        <button @click="cancelDelete" class="px-4 py-2 border border-blue-950 hover:bg-blue-950 hover:text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md mr-2">Cancel</button>
+      <div class="flex justify-end gap-4 mt-4">
+        <button @click="cancelDelete" class="px-4 py-[8px] w-20 border border-blue-950 bg-blue-950 text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md">Cancel</button>
         <button @click="confirmDelete"
-          class="px-4 py-2 w-20 flex justify-center items-center border border-blue-950 bg-blue-950 text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md"
+          class=" px-4 py-[8px] border border-blue-950 hover:bg-blue-950 w-20 flex justify-center items-center hover:text-white hover:bg-opacity-95 transition ease-in-out duration-300 rounded-md"
           :class="{ 'opacity-60 pointer-events-none': loadingAnimation }">
           <span v-if="!loadingAnimation">Delete</span>
           <div class="flex flex-row space-x-4" v-else>
-            <div class="w-6 h-6 rounded-full animate-spin border border-solid border-white border-t-transparent"></div>
+            <div class="w-6 h-6 rounded-full animate-spin border border-solid border-blue-950 border-t-transparent"></div>
           </div>
         </button>
 
--- src/components/TodoListModal.vue
+++ src/components/TodoListModal.vue
@@ -2,28 +2,31 @@
   <div>
     <transition name="modal-fade" appear>
       <div v-if="isModalOpen" class="fixed inset-0 z-50 flex items-center justify-end bg-black bg-opacity-50"  @click="$emit('closeModal')">
-        <div class="bg-blue-950 sm:max-w-[420px] w-full sm:ml-0 ml-10 h-full" @click.stop>
-          <div class="border-white border-b flex justify-between px-5 py-3">
-            <h1 class="text-white text-xl">Add Todo</h1>
+        <div class="bg-white sm:max-w-[420px] w-full sm:ml-0 ml-10 h-full" @click.stop>
+          <div class="border-blue-950 border-b flex justify-between px-5 py-3">
+            <h1 class="text-blue-950 text-xl">Add Todo</h1>
             <svg @click="$emit('closeModal')" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
               stroke-width="1.5" stroke="currentColor"
-              class="text-white w-6 h-6 cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+              class="text-blue-950 w-6 h-6 cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
           </div>
           <div class="px-5 mt-5">
             <div>
               <input v-model="inputAdd" type="text"
-                class="bg-white shadow w-full focus:outline-none text-blue-950 px-4 py-2 rounded-lg placeholder:text-blue-950 text-lg"
+                class="bg-blue-950 shadow w-full focus:outline-none text-white px-4 py-2 rounded-lg placeholder:text-white text-lg"
                 placeholder="Add Todo">
             </div>
-            <div class="flex justify-end mt-5">
+          
+          </div>
+          <div class="fixed bottom-5 px-5 border-t pt-3 w-[22%]">
+             <div>
               <button @click="handleAdd"
-                class="bg-white px-4 py-2 mx-10 w-full text-blue-950 rounded-lg cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+                class="bg-blue-950 px-4 py-2 w-full text-white rounded-lg cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
                 Add
               </button>
+             </div>
             </div>
-          </div>
         </div>
       </div>
     </transition>
