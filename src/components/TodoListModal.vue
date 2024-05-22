4941bff6a9326bd9a8d589f801270cfc204dd2aa
Ismail Memon <iamismailmemon@gmail.com>
2024-05-22 15:13:05 -0700

    first commit

--- src/components/TodoListModal.vue
+++ src/components/TodoListModal.vue
@@ -5,29 +5,30 @@
         @touchend="handleTouchEnd" v-if="isModalOpen"
         class="fixed inset-0 z-50 flex items-center justify-end bg-black bg-opacity-50">
         <div class="bg-white sm:max-w-[450px] w-full sm:ml-0 ml-14 h-full" @click.stop>
-          <div class="border-blue-950 border-b flex justify-between px-5 py-3">
-            <h1 class="text-blue-950 text-xl">Add Todo</h1>
-            <svg @click="$emit('closeModal')" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-              stroke-width="1.5" stroke="currentColor"
-              class="text-blue-950 w-6 h-6 cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          </div>
-          <div class="px-5 mt-5">
-            <div>
-              <input v-model="inputAdd" type="text"
-                class="bg-blue-950 shadow w-full focus:outline-none text-white px-4 py-2 rounded-lg placeholder:text-white text-lg"
-                placeholder="Add Todo">
+          <div class="h-full">
+            <div class="border-b flex justify-between items-center px-5 py-3">
+              <h1 class="text-blue-950 text-2xl">Add Todo</h1>
+              <svg @click="$emit('closeModal')" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke-width="1.5" stroke="currentColor"
+                class="text-blue-950 w-6 h-6 cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <div class=" mt-5 h-full">
+              <div class="px-5">
+                <input v-model="inputAdd" type="text"
+                  class="border w-full focus:outline-none px-4 py-2 rounded-lg  text-lg" placeholder="Type a Todo">
+              </div>
+              <div class="w-full sm:h-[calc(100%-145px)] h-[calc(100%-140px)] flex items-end">
+<div class="border-t w-full px-5 sm:pt-5 pt-4">
+  <button @click="handleAdd"
+                  class="bg-blue-950 sm:text-xl text-lg px-4 py-2 w-full text-white rounded-lg cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+                  Add
+                </button>
+</div>
+              </div>
             </div>
 
-          </div>
-          <div class="fixed bottom-5 px-5 border-t pt-5 w-[22%]">
-            <div>
-              <button @click="handleAdd"
-                class="bg-blue-950 text-xl px-4 py-2 w-full text-white rounded-lg cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
-                Add
-              </button>
-            </div>
           </div>
         </div>
       </div>
@@ -38,25 +39,22 @@
 <script>
 export default {
   props: {
-    addTodo: Function, // Corrected to Function type
+    addTodo: Function,
     isModalOpen: Boolean,
   },
   data() {
     return {
-
-      inputAdd: "" // Moved inside data function
+      inputAdd: ""
     };
   },
   methods: {
     handleTouchStart(event) {
       this.touchStartX = event.touches[0].clientX;
-
     },
     handleTouchMove(event) {
       const touchMoveX = event.touches[0].clientX;
       if (touchMoveX - this.touchStartX > 50) {
         // If sliding right, hide swipe indicator
-
       }
     },
     handleTouchEnd(event) {
@@ -65,9 +63,7 @@ export default {
         // If the touch ended with a left swipe (positive difference in X coordinates)
         this.$emit('closeModal');
       }
-
     },
-
     handleAdd() {
       if (this.inputAdd.length === 0) { } else {
         this.addTodo.push(
@@ -76,7 +72,7 @@ export default {
             isTrueRecycle: true
           },
         );
-        this.inputAdd = ""; // Clear input field after adding todo
+        this.inputAdd = "";
         this.$emit('closeModal')
       }
     }
